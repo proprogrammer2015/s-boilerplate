1df80cf500a32cc245d12cbe64937ca98dc3d5f4
Jacek Mankowski <it.developer.1992@gmail.com>
2019-03-02 23:22:37 +0100

    Updated dependencies.

--- watcher.config.js
+++ watcher.config.js
@@ -1,12 +1,22 @@
-const { Html, Js } = require('svelte-module-combine/lib/index');
+const color = require('colors');
+const { Html, Js } = require('svelte-module-combine');
 const { Sass } = require('./transformations/Sass');
 
 exports.patterns = [
     './src/**'
 ];
 exports.output = './output';
+exports.outputExtension = 'html';
 exports.processors = [
     new Html(),
     new Js(),
     new Sass()
 ];
+exports.entrypoints = {
+    delete: path => console.log(color.grey(`${path} was removed.`)),
+    error: msg => console.log(color.red(`ERROR: ${msg}`)),
+    compile: (path, all) => console.log(color.green(`${path} was compiled.`)),
+    change: (path, all) => console.log(color.yellow(`${path} was changed.`)),
+    add: (path, all) => console.log(color.green(`${path} was added.`)),
+    ready: (_, all) => console.table(color.rainbow(`[SVELTE MODULE COMBINE]`))
+}
